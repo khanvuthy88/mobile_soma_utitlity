c6ea22241392073a3eb0d0ffac7afffd2545ada1
khanvuthy88 <khanvuthy88@gmail.com>
2023-03-17 14:18:33 +0700

    Add provider to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
 import 'package:soma_utility/pages/home_page.dart';
+import 'package:soma_utility/providers/zone_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      MultiProvider(
+        providers: [
+         // ChangeNotifierProxyProvider<ZoneProvider>(
+         //   create: (context)=> ZoneProvider(),
+         //   update: update)
+        ],
+        child: const MyApp(),
+      ));
 }
 
 class MyApp extends StatelessWidget {
