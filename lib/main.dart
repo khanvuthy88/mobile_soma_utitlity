dc2a8929da471edd0ab7e9bd978157ac54f99ca9
khanvuthy88 <khanvuthy88@gmail.com>
2023-03-23 12:48:45 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 import 'package:soma_utility/pages/home_page.dart';
 import 'package:soma_utility/providers/zone_provider.dart';
