d45714fdd0e6de9851c3dbaf41e988982cc11354
khanvuthy88 <khanvuthy88@gmail.com>
2023-03-24 19:30:48 +0700

    Update

--- lib/pages/customer_page.dart
+++ lib/pages/customer_page.dart
@@ -11,93 +11,101 @@ class CustomerPage extends StatefulWidget {
 }
 
 class _CustomerPageState extends State<CustomerPage> {
+
+  late bool _isLastPage;
+  late int _pageNumber;
+  late bool _error;
+  late bool _loading;
+  final int _numberOfPostsPerRequest = 10;
+  late List<CustomerModelData> customerList;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageNumber = 0;
+    customerList = [];
+    _isLastPage = false;
+    _loading = true;
+    _error = false;
+    fetchData();
+  }
+
+  void fetchData() async {
+    final data = Provider.of<CustomerProvider>(context, listen: false).customerList;
+    setState(() {
+      customerList.addAll(data);
+      _loading = false;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CustomerProvider(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('អតិថិជនទាំងអស់'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  debugPrint("clicked");
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("អតិថិជនទាំងអស់"),
+      ),
+      body: _loading ? const Center(child: CircularProgressIndicator()) : Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                debugPrint("clicked");
+              },
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
                     color: Colors.blue,
                     borderRadius: BorderRadius.circular(4)
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Icon(Icons.filter_alt_rounded, color: Colors.white,),
-                      const SizedBox(width: 10),
-                      Text("FILTER", style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(Icons.filter_alt_rounded, color: Colors.white,),
+                    const SizedBox(width: 10),
+                    Text("FILTER", style: Theme.of(context).textTheme.titleMedium!.copyWith(
                         color: Colors.white
-                      ),)
-                    ],
-                  ),
+                    ),)
+                  ],
                 ),
               ),
-              FutureBuilder(
-                future: context.watch<AppDb>().getCustomers(),
-                builder: (context, snapshot) {
-                  final List<CustomerModelData>? customers = snapshot.data;
-                  print(customers);
-                  if(snapshot.connectionState != ConnectionState.done){
-                    return const Center(
-                      child: CircularProgressIndicator(),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: customerList.length,
+                  itemBuilder: ((context, index) {
+                    final customer = customerList[index];
+                    return Card(
+                      color: customer.newConsumption != 0 ? Colors.blue.shade200 : Colors.white,
+                      child: ListTile(
+                        leading: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(customer.name),
+                            Text(customer.meter.toString()),
+                          ],
+                        ),
+                        title: Text(customer.village),
+                        subtitle: Text(customer.zone),
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text('អំណានថ្មី: ${customer.newConsumption}'),
+                            Text('អំណានចាស់: ${customer.previousConsumption}'),
+                          ],
+                        ),
+                      ),
                     );
-                  }
-                  if(snapshot.hasError){
-                    return Center(
-                      child: Text(snapshot.error.toString()),
-                    );
-                  }
-                  if(customers != null){
-                    return ListView.builder(
-                        itemCount: customers.length,
-                        itemBuilder: (context, index){
-                          final customer = customers[index];
-                          return Card(
-                            color: customer.newConsumption != 0 ? Colors.blue.shade200 : Colors.white,
-                            child: ListTile(
-                              leading: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(customer.name),
-                                  Text(customer.meter.toString()),
-                                ],
-                              ),
-                              title: Text(customer.village),
-                              subtitle: Text(customer.zone),
-                              trailing: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text('អំណានថ្មី: ${customer.newConsumption}'),
-                                  Text('អំណានចាស់: ${customer.previousConsumption}'),
-                                ],
-                              ),
-                            ),
-                          );
-                        }
-                    );
-                  }
-                  return const Center(
-                    child: Text("No Data found"),
-                  );
-                },
+                  })
               ),
-            ],
-          )
+            ),
+          ],
         ),
       ),
     );
